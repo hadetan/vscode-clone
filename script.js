cb8f7e2463d8729d34535f720418b96bcf6bb067
hadetan <aquibsyed83@gmail.com>
2024-05-02 20:51:13 +0530

    7-2 commit perfection on 800px

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// const init = function(){
-//     gsap.registerPlugin(ScrollTrigger);
-
-// // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-// const locoScroll = new LocomotiveScroll({
-//   el: document.querySelector("#main"),
-//   smooth: true
-// });
-// // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-// locoScroll.on("scroll", ScrollTrigger.update);
-
-// // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
-// ScrollTrigger.scrollerProxy("#main", {
-//   scrollTop(value) {
-//     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-//   }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-//   getBoundingClientRect() {
-//     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-//   },
-//   // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-//   pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-// });
-
-
-
-
-// // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-// ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-// ScrollTrigger.refresh();
-// }
-// init();
-
 var tl = gsap.timeline();
 tl
 .from("#download #dleft #dtext h1", {
@@ -194,22 +159,6 @@ logo12.addEventListener("mouseleave", function(){
     logo12.style.scale = 1;
 })
 
-// var downl = document.querySelector("#download #dleft #dtext");
-// var downr = document.querySelector("#download #dright #vsimg");
-
-// downl.addEventListener("mousemove", function(){
-//     downl.style.scale = 1.1;
-// })
-// downl.addEventListener("mouseleave", function(){
-//     downl.style.scale = 1;
-// })
-// downr.addEventListener("mousemove", function(){
-//     downr.style.scale = 1.1;
-// })
-// downr.addEventListener("mouseleave", function(){
-//     downr.style.scale = 1;
-// })
-
 var ileft = document.querySelector("#ipart #ileft img");
 ileft.addEventListener("mousemove", function(){
     ileft.style.scale = 1.2;
@@ -240,4 +189,67 @@ dpleft.addEventListener("mousemove", function(){
 })
 dpleft.addEventListener("mouseleave", function(){
     dpleft.style.scale = 1.1;
+})
+
+var nav = document.querySelector("#nav2");
+var ul = document.querySelector("#nav2 ul");
+var btn = document.querySelector("#nav2 button");
+var menu = document.querySelector("#nav #menu");
+
+var flag = 0;
+// menu.addEventListener("click", function(){
+//     if (flag == 0) {
+//         nav.style.opacity = 1;
+//         ul.style.opacity = 1;
+//         btn.style.opacity = 1;
+//         flag = 1;
+//     } else {
+//         nav.style.opacity = 0;
+//         ul.style.opacity = 0;
+//         btn.style.opacity = 0;
+//         flag = 0;
+//     }
+// })
+
+
+menu.addEventListener("click", function(){
+    if (flag == 0) {
+        gsap.to("#nav2", {
+            duration: 0.5,
+            // delay: -0.9,
+            height: "500px"
+        })
+        gsap.to("#nav2 ul", {
+            duration: 0.5,
+            opacity: 1,
+            x: 10,
+            delay: 0.1,
+            width: "7vw",
+        })
+        gsap.to("#nav2 button", {
+            duration: 0.5,
+            opacity: 1,
+            delay: 0.2,
+        })
+        flag = 1;
+    } else {
+        gsap.to("#nav2", {
+            duration: 0.5,
+            // delay: -0.9,
+            height: "0px"
+        })
+        gsap.to("#nav2 ul", {
+            duration: 0.5,
+            opacity: 0,
+            x: -10,
+            delay: -2,
+            width: "0vw",
+        })
+        gsap.to("#nav2 button", {
+            duration: 0.5,
+            opacity: 0,
+            delay: 0.2,
+        })
+        flag = 0;
+    }
 })
